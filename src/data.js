1202f3636ef1f2a82165800a8166b7302b796062
Elibabah <delias16.hernandez@gmail.com>
2021-10-28 23:31:47 -0500

    fetch funcionando en consola

--- src/data.js
+++ src/data.js
@@ -1,7 +1,19 @@
 //---//> Obtener aquí los datos del JSON local y luego exportarlos para usarlos en el módulo app.js <//----//
 
+
+// Exportación de módulos
+export let getCardsfromAPI = (tarjetaParaBuscar) => {}
+
+
+
+
 // traer la data del JSON con un fetch
-fetch()
+fetch("../data/memory.json")
     .then((response) => response.json()) // traer el json
     .then((data) => iterarTarjetas(data)) // traer data y dar a variable
     .catch((error) => console.log(error)) // atrapar error
+
+// variable del área de tarjetas
+let iterarTarjetas = (data) => {
+    console.log(data.memory_Game) // consologear el data del JSON
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 //-----------//> Eventos DOM. Obtener los valores de búsqueda para procesarlos en app.js <//------------ //
+
+
+// Importación de módulos. Recibe el EXPORT de data.js
+import { getCardsfromAPI } from "./data.js";
