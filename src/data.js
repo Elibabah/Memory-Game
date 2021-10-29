dda9bf72876bee36d9d8c2e67c736359b6387910
Elibabah <delias16.hernandez@gmail.com>
2021-10-28 22:51:01 -0500

    trayendo la data del json con fetch en data.js

--- src/data.js
+++ src/data.js
@@ -1,1 +1,7 @@
 //---//> Obtener aquí los datos del JSON local y luego exportarlos para usarlos en el módulo app.js <//----//
+
+// traer la data del JSON con un fetch
+fetch()
+    .then((response) => response.json()) // traer el json
+    .then((data) => iterarTarjetas(data)) // traer data y dar a variable
+    .catch((error) => console.log(error)) // atrapar error
