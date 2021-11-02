89ff553088bde2df32aa50a785c475730f88cf83
Elibabah <delias16.hernandez@gmail.com>
2021-11-02 17:06:35 -0600

    función al click sobre imagen Ok

--- src/data.js
+++ src/data.js
@@ -22,8 +22,8 @@ let iterarTarjetas = (data) => { // iterar el objeto
         mazo.innerHTML += // imprimir en HTML
             `
             <div class="card-container">
-                <div class="cardPortrait" id="card"> 
-                    <div class="card-cover"></div> 
+                <div class="cardPortrait" > 
+                    <div class="card-cover" onclick="testScope('${ficha.name}')"></div> 
                     <div class="card-back" id='${ficha.name}'></div>
                 </div>
             </div>
@@ -35,32 +35,31 @@ let iterarTarjetas = (data) => { // iterar el objeto
         caraCard.style.backgroundSize = "cover"
 
         // Función para voltear tarjeta al click
-        const cardPortrait = document.getElementById("card")
-        console.log(card) // OK, el ID "card" lo trae cada imagen
+        // const cardPortrait = document.getElementById(`${ficha.name}`)
+        // console.log(cardPortrait) // OK, el ID lo trae cada imagen
 
-        cardPortrait.addEventListener("click", flipCard)
-        console.log(addEventListener, flipCard) //OK, sí me da las función escuchador y flipCard
-            /*cardPortrait.style.transform = "rotateY(180deg)"*/
+        // cardPortrait.addEventListener("click", flipCard)
+        // console.log(addEventListener, flipCard) //OK, sí me da las función escuchador y flipCard
+        /*cardPortrait.style.transform = "rotateY(180deg)"*/
 
-        function flipCard() {
-            // AQUÍ ESTÁ EL PROBLEMA: el toggle no me devuelve la clase flipCard a todas las tags cardPortrait de c/ficha; sólo a la primera
-            cardPortrait.classList.toggle("flipCard");
-        }
-
-
-        //Solución a implementar
-        /*    function flipCard() {
-                let selected = this.dataset.id;
-                cardsSelected.push(cardArray[selected].name);
-                cardsId.push(selected);
-                this.classList.add("flip");
-                this.setAttribute("src", cardArray[selected].img);
-                if (cardsId.length === 2) {
-                    setTimeout(checkForMatch, 500);
-                }
-            } */
-
-
-
+        // function flipCard() {
+        // AQUÍ ESTÁ EL PROBLEMA: el toggle no me devuelve la clase flipCard a todas las tags cardPortrait de c/ficha; sólo a la primera
+        //    cardPortrait.classList.toggle("flipCard");
     }
+
+
+    //Solución a implementar
+    /*    function flipCard() {
+            let selected = this.dataset.id;
+            cardsSelected.push(cardArray[selected].name);
+            cardsId.push(selected);
+            this.classList.add("flip");
+            this.setAttribute("src", cardArray[selected].img);
+            if (cardsId.length === 2) {
+                setTimeout(checkForMatch, 500);
+            }
+        } */
+
+
+
 }
