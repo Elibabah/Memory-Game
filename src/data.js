288c85e70c9fea73aa53fd1d0309742054ff0d7d
Elibabah <delias16.hernandez@gmail.com>
2021-11-02 05:35:59 -0600

    volteando tarjetas con flipCard

--- src/data.js
+++ src/data.js
@@ -22,17 +22,29 @@ let iterarTarjetas = (data) => { // iterar el objeto
         mazo.innerHTML += // imprimir en HTML
             `
             <div class="card-container">
-            <div class="card-portrait">
-            <div class="card-cover"></div> 
-            <div class="card-back" id='${ficha.name}'>
-            <p class="value" id='${ficha.name}_value'</p>
+                <div class="cardPortrait" id="card"> 
+                    <div class="card-cover"></div> 
+                    <div class="card-back" id='${ficha.name}'></div>
+                </div>
             </div>
-            </div>
-        </div>
         `
 
+        // declaración para dar estilos dinámicamente por ID-nombre a la imagen oculta        
         let caraCard = document.getElementById(`${ficha.name}`)
         caraCard.style.backgroundImage = "url(" + ` ${ficha.image}` + ")"
         caraCard.style.backgroundSize = "cover"
+
+        // Función para voltear tarjeta al click
+        const cardPortrait = document.getElementById("card")
+        console.log(card) // OK, el ID "card" lo trae cada imagen
+
+        cardPortrait.addEventListener("click", flipCard)
+        console.log(addEventListener, flipCard) //OK, sí me da las función escuchador y flipCard
+            /*cardPortrait.style.transform = "rotateY(180deg)"*/
+
+        function flipCard() {
+            // AQUÍ ESTÁ EL PROBLEMA: el toggle no me devuelve la clase flipCard a todas las tags cardPortrait de c/ficha; sólo a la primera
+            cardPortrait.classList.toggle("flipCard");
+        }
     }
 }
